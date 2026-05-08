1e202de7abf9d6cd77fc548c6e542721309831d4
Mubarak Tijani <mubarak.tijani@amalitechtraining.org>
2026-05-08 10:22:59 +0000

    feat(lab-1.1): add GlueServiceRole, LambdaExecutionRole, RedshiftIAMRole
    - GlueServiceRole: AWSGlueServiceRole + S3 + CloudWatch
    - LambdaExecutionRole: Basic execution + S3 + DynamoDB + Kinesis
    - RedshiftIAMRole: S3 full access + CloudWatch for COPY/UNLOAD ops
    - All roles use least-privilege AWS managed policies

--- lab-1.1-iam-setup/iam.tf
+++ lab-1.1-iam-setup/iam.tf
@@ -46,3 +46,93 @@ resource "aws_iam_role_policy_attachment" "de_cloudwatch" {
   role       = aws_iam_role.data_engineer_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
+
+# Glue Service Role
+resource "aws_iam_role" "glue_service_role" {
+  name        = "GlueServiceRole-tf"
+  description = "Service role for AWS Glue jobs"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "glue.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "glue_s3" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "glue_cloudwatch" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+# Lambda Execution Role
+resource "aws_iam_role" "lambda_execution_role" {
+  name        = "LambdaExecutionRole-tf"
+  description = "Execution role for Lambda functions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_kinesis" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
+}
+
+# Redshift IAM Role
+resource "aws_iam_role" "redshift_iam_role" {
+  name        = "RedshiftIAMRole-tf"
+  description = "Service role for Redshift to read/write to S3"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "redshift.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "redshift_s3" {
+  role       = aws_iam_role.redshift_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "redshift_cloudwatch" {
+  role       = aws_iam_role.redshift_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
